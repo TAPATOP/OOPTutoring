ca204e202ab4c36d810c8f56d895f945b11f86db
Vasil Pashov <vasil.pashov1@gmail.com>
2016-03-29 19:53:54 +0300

    Strings added move constructor and operator. Bugs fixed.

--- 2016/04 TemplatesAndExercises/String/String.cpp
+++ 2016/04 TemplatesAndExercises/String/String.cpp
@@ -1,25 +1,31 @@
-#include "String.h"
+#include "string.h"
+
+using namespace std;
 
 namespace MString {
 
-    void String::CopyCharArr(char* source, char* dest, int fromIndex, uint size) {
-        size = size ? size : GetCharArrLength(source);
-        for(uint i = fromIndex, j = 0; i < fromIndex + size; i++, j++) {
+    void String::copy_char_arr(char* source, char* dest, int from_index, uint size) {
+        size = size ? size : get_char_arr_length(source);
+        for(uint i = from_index, j = 0; i < from_index + size; i++, j++) {
             dest[i] = source[j];
         }
     }
 
     String::String(){};
 
-    String::String(const char *inputString) {
-        Set(inputString);
+    String::String(const char *input_string) {
+        set(input_string);
     }
 
-    String::String(String &inputString) {
-        Set(inputString.m_string, inputString.m_size);
+    String::String(const String &input_string) {
+        set(input_string.m_string, input_string.m_size);
     }
 
-    int String::Size() {
+    String::String(String&& other) : m_size{other.m_size}, m_string{other.m_string} {
+        other.m_string = nullptr;
+    }
+
+    uint String::size() {
         return m_size;
     }
 
@@ -31,59 +37,66 @@ namespace MString {
     }
 
     std::istream& operator>> (std::istream &in, String &a) {
-        char *input = new char[8], c, *temp = nullptr;
-        int i = 0;
+        uint i = 0, capacity = 8;
+        char *input = new char[capacity], c, *temp = nullptr;
         while(c != ' ' && c != '\t' && c != '\n') {
             in.get(c);
-            if(i >= 8) {
+            if(i >= capacity) {
                 temp = new char[i];
-                String::CopyCharArr(input, temp, 0, i);
+                capacity*= 2;
+                String::copy_char_arr(input, temp, 0, i);
                 delete [] input;
-                input = new char[i*2];
-                String::CopyCharArr(temp, input, 0, i);
-                delete temp;
+                input = new char[capacity];
+                String::copy_char_arr(temp, input, 0, i);
+                delete[] temp;
                 temp = nullptr;
-                i*=2;
             }
-            input[i] = c;
-            i++;
+            input[i++] = c;
         }
-        a.Set(input, i-1);
+        a.set(input, i-1);
         delete [] input;
         return in;
     }
 
     String& String::operator= (const char *a) {
-        Set(a);
+        set(a);
         return *this;
     }
 
     String& String::operator= (const String &a) {
-        Set(a.m_string, a.m_size);
+        set(a.m_string, a.m_size);
+        return *this;
+    }
+
+    String& String::operator= (String &&a) {
+        delete[] m_string;
+        m_size = a.m_size;
+        m_string = a.m_string;
+        a.m_string = nullptr;
         return *this;
     }
 
     String operator+(const String &a, const String &b) {
-        unsigned int la = a.m_size, lb = b.m_size;
-        char *newStr = new char[la+lb];
-        String::CopyCharArr(a.m_string, newStr, 0, la);
-        String::CopyCharArr(b.m_string, newStr, la, lb);
-        String s(newStr);
-        return s;
+        uint la = a.m_size, lb = b.m_size;
+        char *new_str = new char[la + lb + 1];
+        String::copy_char_arr(a.m_string, new_str, 0, la);
+        String::copy_char_arr(b.m_string, new_str, la, lb);
+        String s(new_str);
+        return std::move(s);
     }
 
     String& String::operator+= (const String& other) {
-        uint newSize = m_size + other.m_size, oldSize = m_size;
-        char *old = new char[oldSize];
-        String::CopyCharArr(m_string, old, 0, oldSize);
-        Resize(newSize);
-        String::CopyCharArr(old, m_string, 0, oldSize);
-        String::CopyCharArr(other.m_string, m_string, oldSize, other.m_size);
+        uint new_size = m_size + other.m_size, old_size = m_size;
+        char *old = new char[old_size];
+        String::copy_char_arr(m_string, old, 0, old_size);
+        resize(new_size);
+        String::copy_char_arr(old, m_string, 0, old_size);
+        String::copy_char_arr(other.m_string, m_string, old_size, other.m_size);
         return *this;
     }
 
     bool operator== (const String& a, const String& b) {
-        if(!String::Compare(a,b)) return true;
+        if(!String::compare(a,b)) return true;
         return false;
     }
 
@@ -91,9 +104,12 @@ namespace MString {
         return !(a==b);
     }
 
-    short String::Compare(const String& a, const String& b) {
-        uint i = 0, minLength = a.m_size <= b.m_size ? a.m_size : b.m_size;
-        while(i < minLength && a.m_string[i] == b.m_string[i]) i++;
+    short String::compare(const String& a, const String& b) {
+        if(a.m_string == nullptr && b.m_string == nullptr) return 0;
+        if(a.m_string == nullptr) return 1;
+        if(b.m_string == nullptr) return -1;
+        uint i = 0, min_length = a.m_size <= b.m_size ? a.m_size : b.m_size;
+        while(i < min_length && a.m_string[i] == b.m_string[i]) i++;
         if(a.m_string[i] > b.m_string[i]) return 1; //a > b
         if(a.m_string[i] < b.m_string[i]) return -1; //a < b
         if(a.m_size == b.m_size && a.m_size == i) return 0; //a == b
@@ -101,31 +117,33 @@ namespace MString {
     }
 
     bool operator> (const String& a, const String& b) {
-        if(String::Compare(a,b) == 1) return true;
+        if(String::compare(a,b) == 1) return true;
         return false;
     }
 
      bool operator< (const String& a, const String& b) {
-        if(String::Compare(a,b) == -1) return true;
+        if(String::compare(a,b) == -1) return true;
         return false;
     }
 
      bool operator<= (const String& a, const String& b) {
-        if(String::Compare(a,b) < 1) return true;
+        if(String::compare(a,b) < 1) return true;
         return false;
     }
 
      bool operator>= (const String& a, const String& b) {
-        if(String::Compare(a,b) > -1) return true;
+        if(String::compare(a,b) > -1) return true;
         return false;
     }
 
     char& String::operator[] (const int index) {
         //Not safe if index > 0 || index < 0 exception should be thrown
+        //Or implement at(const uint) function
         return m_string[index];
     }
 
-    uint String::GetCharArrLength(const char *s) {
+    uint String::get_char_arr_length(const char *s) {
+        if(s == nullptr) return 0;
         uint l = 0;
         while(s[l]) {
             l++;
@@ -133,15 +151,15 @@ namespace MString {
         return l;
     }
 
-    void String::Resize(uint newSize) {
+    void String::resize(uint new_size) {
         delete[] m_string;
-        m_string = new char[newSize];
-        m_size = newSize;
+        m_string = new char[new_size+1];
+        m_size = new_size;
     }
 
-    void String::Set(const char *s, uint size) {
-        size = size != 0 ? size : GetCharArrLength(s);
-        Resize(size);
+    void String::set(const char *s, uint size) {
+        size = size ? size : get_char_arr_length(s);
+        resize(size);
         for(uint i = 0; i < size; i++) {
             m_string[i] = s[i];
         }
--- 2016/04 TemplatesAndExercises/String/String.h
+++ 2016/04 TemplatesAndExercises/String/String.h
@@ -4,17 +4,21 @@
 #include <iostream>
 
 namespace MString {
+
     typedef unsigned int uint;
+
     class String {
     public:
         String();
         String(const char*);
-        String(String&);
-        int Size();
+        String(const String&);
+        String(String&&);
+        uint size();
         friend std::ostream& operator<< (std::ostream&, const String&);
-        friend std::istream& operator>> (std::istream &in, String &a);
+        friend std::istream& operator>> (std::istream&,  String&);
         String& operator+= (const String&);
         String& operator=(const String&);
+        String& operator=(String&&);
         String& operator=(const char*);
         char& operator[] (const int);
         friend String operator+(const String&, const String&);
@@ -24,16 +28,15 @@ namespace MString {
         friend bool operator< (const String&, const String&);
         friend bool operator>= (const String&, const String&);
         friend bool operator<= (const String&, const String&);
-        static void CopyCharArr(char*, char*, int = 0, unsigned int = 0);
         ~String();
     private:
-        char *m_string = nullptr;
+        static void copy_char_arr(char*, char*, int = 0, uint = 0);
+        static uint get_char_arr_length(const char*);
+        static short compare(const String&, const String&);
+        void resize(uint);
+        void set(const char*, uint = 0);
         uint m_size = 0;
-        static uint GetCharArrLength(const char*);
-        void Resize(uint);
-        static short Compare(const String&, const String&);
-        void Set(const char*, uint = 0);
-
+        char *m_string = nullptr;
     };
 }
 
